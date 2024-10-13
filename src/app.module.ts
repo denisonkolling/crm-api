2b8dd80ea2a5ef2cc169157eb5ca9045aed962aa
Denison Perin Kolling <denison@ibs.inf.br>
2024-10-12 21:43:37 -0300

    add main entities resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ContactsModule } from './contacts/contacts.module';
+import { LeadsModule } from './leads/leads.module';
+import { OpportunitiesModule } from './opportunities/opportunities.module';
+import { TasksModule } from './tasks/tasks.module';
+import { CampaignsModule } from './campaigns/campaigns.module';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ContactsModule, LeadsModule, OpportunitiesModule, TasksModule, CampaignsModule, EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
