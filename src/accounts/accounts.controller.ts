b748188dcc34327569cc54f74933782b1b5735f5
Denison Perin Kolling <denison@ibs.inf.br>
2024-10-14 16:30:18 -0300

    feat(account): implement pagination and sorting for findAll method

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -1,33 +1,49 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 import { CreateAccountDto } from './dto/create-account.dto';
 import { UpdateAccountDto } from './dto/update-account.dto';
+import { ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 
 @Controller('accounts')
+@ApiTags('accounts')
 export class AccountsController {
-  constructor(private readonly accountsService: AccountsService) {}
+  constructor(private readonly accountsService: AccountsService) { }
 
   @Post()
+  @ApiOperation({ summary: 'Create a new account' })
   create(@Body() createAccountDto: CreateAccountDto) {
     return this.accountsService.create(createAccountDto);
   }
 
   @Get()
-  findAll() {
-    return this.accountsService.findAll();
+  @ApiOperation({ summary: 'Get all accounts with pagination and sorting options' })
+  @ApiQuery({ name: 'page', required: false, type: Number, description: 'Page number to retrieve (default is 1)' })
+  @ApiQuery({ name: 'limit', required: false, type: Number, description: 'Number of accounts to retrieve per page (default is 10)' })
+  @ApiQuery({ name: 'sortBy', required: false, type: String, description: 'Field by which to sort the accounts (default is "id")' })
+  @ApiQuery({ name: 'sortOrder', required: false, enum: ['asc', 'desc'], description: 'Order in which to sort the accounts (default is "asc")' })
+  findAll(
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
+    @Query('sortBy') sortBy: string = 'id',
+    @Query('sortOrder') sortOrder: 'asc' | 'desc' = 'asc'
+  ) {
+    return this.accountsService.findAll({ page, limit, sortBy, sortOrder });
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get an account by id' })
   findOne(@Param('id') id: string) {
     return this.accountsService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update an account by id' })
   update(@Param('id') id: string, @Body() updateAccountDto: UpdateAccountDto) {
     return this.accountsService.update(+id, updateAccountDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete an account by id' })
   remove(@Param('id') id: string) {
     return this.accountsService.remove(+id);
   }
