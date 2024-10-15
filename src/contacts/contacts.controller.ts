4c8c8a7e5d718cb314e64a7f50edae144295e2f6
Denison Perin Kolling <denison@ibs.inf.br>
2024-10-15 08:02:30 -0300

    feat(contact): implement contact CRUD operations

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { ContactsService } from './contacts.service';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
+import { ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 
 @Controller('contacts')
+@ApiTags('contacts')
 export class ContactsController {
-  constructor(private readonly contactsService: ContactsService) {}
+  constructor(private readonly contactsService: ContactsService) { }
 
   @Post()
   create(@Body() createContactDto: CreateContactDto) {
@@ -13,8 +15,20 @@ export class ContactsController {
   }
 
   @Get()
-  findAll() {
-    return this.contactsService.findAll();
+  @ApiOperation({ summary: 'Get all contacts' })
+  @ApiQuery({ name: 'page', required: false, type: Number, description: 'Page number to retrieve (default is 1)' })
+  @ApiQuery({ name: 'limit', required: false, type: Number, description: 'Number of contacts to retrieve per page (default is 10)' })
+  @ApiQuery({ name: 'name', required: false, type: String, description: 'Name to filter contacts by' })
+  @ApiQuery({ name: 'sortBy', required: false, type: String, description: 'Field by which to sort the contacts (default is "id")' })
+  @ApiQuery({ name: 'sortOrder', required: false, enum: ['asc', 'desc'], description: 'Order in which to sort the contacts (default is "asc")' })
+  findAll(
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
+    @Query('name') name?: string,
+    @Query('sortBy') sortBy: string = 'id',
+    @Query('sortOrder') sortOrder: 'asc' | 'desc' = 'asc'
+  ) {
+    return this.contactsService.findAll({ page, limit, sortOrder, sortBy, name });
   }
 
   @Get(':id')
--- src/contacts/contacts.service.ts
+++ src/contacts/contacts.service.ts
@@ -1,26 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
+import { Contact } from './entities/contact.entity';
+import { EntityManager } from '@mikro-orm/postgresql';
 
 @Injectable()
 export class ContactsService {
-  create(createContactDto: CreateContactDto) {
-    return 'This action adds a new contact';
+
+  constructor(
+    private readonly em: EntityManager,
+  ) { }
+
+  async create(createContactDto: CreateContactDto) {
+    const contact = new Contact();
+    this.em.assign(contact, createContactDto);
+    await this.em.persistAndFlush(contact);
+    return contact;
   }
 
-  findAll() {
-    return `This action returns all contacts`;
+  async findAll({ page, limit, sortBy, sortOrder, name }: { page: number, limit: number, sortBy: string, sortOrder: 'asc' | 'desc', name?: string }): Promise<Contact[]> {
+    const offset = (page - 1) * limit;
+    const filters = name ? { firstName: { $ilike: `%${name}%` } } : {};
+    return this.em.find(Contact, filters, {
+      limit,
+      offset,
+      orderBy: { [sortBy]: sortOrder }
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} contact`;
+  async findOne(id: number) {
+    return this.em.findOne(Contact, id);
   }
 
-  update(id: number, updateContactDto: UpdateContactDto) {
-    return `This action updates a #${id} contact`;
+  async update(id: number, updateContactDto: UpdateContactDto) {
+    const contact = this.em.findOne(Contact, { id });
+    this.em.assign(contact, updateContactDto);
+    await this.em.persistAndFlush(contact);
+    return contact;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} contact`;
+  async remove(id: number) {
+    const contact = await this.em.findOne(Contact, { id });
+
+    if (!contact) {
+      throw new NotFoundException(`Contact with id ${id} not found`);
+    }
+    return {
+      statusCode: 204,
+      message: `Account with id ${id} has been successfully deleted.`,
+    };
   }
 }
